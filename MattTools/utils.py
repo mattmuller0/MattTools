aa7b67725cb0f7a0dd076297e611660a1630dc5c
Matthew Muller <mm12865@nyu.edu>
2023-04-10 17:02:11 -0400

    added utils.py

---
+++ MattTools/utils.py
@@ -0,0 +1,84 @@
+######################################################################
+#
+#                       Utility Functions
+#
+######################################################################
+# Matthew Muller
+# 11/24/2022
+#
+# This file contains functions for modeling and evaluating models.
+
+# Library Imports
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import random
+import sys
+
+
+# Funcition to plot progress bar
+def progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+# Function to set the random seed as a random integer
+def set_random_seed(seed=np.random.randint(1, 10000)):
+    '''
+    Summary: Function to set the random seed for reproducibility
+
+    seed (int) : integer value of the random seed
+    '''
+    print(f'Setting random seed to {seed} for reproducibility.')
+
+    # Set the random seed if it is installed
+    try:
+        import random
+        random.seed(seed)
+    except:
+        pass
+
+    # Set random seed for numpy
+    try:
+        import numpy as np
+        np.random.seed(seed)
+    except:
+        pass
+
+    # Set random seed for sklearn
+    try:
+        import sklearn
+        sklearn.random_state(seed)
+    except:
+        pass
+
+    # Set the random seed for tensorflow if it is installed
+    try:
+        import tensorflow as tf
+        tf.random.set_seed(seed)
+    except:
+        pass
+
+    # Set the random seed for pytorch if it is installed
+    try:
+        import torch
+        torch.manual_seed(seed)
+    except:
+        pass
