ddc727ad37cb176d787ce77e31691c1768a198b2
Matthew Muller <mm12865@nyu.edu>
2023-04-10 15:06:21 -0400

    added init

---
+++ tools/__init__.py
@@ -0,0 +1,21 @@
+"""
+This is the module docstring. It should contain a brief overview of what the module does.
+"""
+
+__version__ = "0.1.0"
+
+# Import necessary modules
+from . import stats
+from . import plotting
+
+# Define any necessary functions or classes
+# ...
+
+# Define any necessary variables
+# ...
+
+# Define any necessary setup or configuration code
+# ...
+
+# Define the __all__ variable to specify which modules are imported when using `from module import *`
+__all__ = ["stats", "plotting"]
