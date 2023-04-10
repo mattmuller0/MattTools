9f2642e6d34c607d3f080e84dbc14a7c4c316d3c
Matthew Muller <mm12865@nyu.edu>
2023-04-10 15:09:28 -0400

    fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import find_packages, setup
 
-setup(name="MattTools",
+setup(
+      name="MattTools",
       version="0.2",
       description="My package",
       url="https://github.com/matttmuller0/MattTools",
       author="Matthew Muller",
       author_email="matt.alex.muller@gmail.com", 
       license="MIT",
-      packages=[find_packages()],
-      install_requires=[],
-      zip_safe=False)
+      packages=find_packages(),
+      install_requires=[]
+      )
