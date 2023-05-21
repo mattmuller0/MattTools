8c50cdec63f0099a856891c69a4d8e7b1e404ef9
Matthew Muller <mm12865@nyu.edu>
2023-05-20 21:58:46 -0400

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MattTools',
-    version='0.3.14',
+    version='0.3.15',
     packages=find_packages(),
     install_requires=[ 
             'numpy',
