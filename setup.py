a8b326864d1e492513788064bfcd6c59b1bbe53b
Matthew Muller <mm12865@nyu.edu>
2024-06-04 23:38:52 -0400

    added PR curve support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MattTools',
-    version='0.4.3',
+    version='0.4.4',
     packages=find_packages(exclude=['tests*']),
     install_requires=[ 
             'numpy',
